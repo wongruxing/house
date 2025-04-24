a01874b33873ee32723d0bde8d299fbd850bea49
wongruxing <112768375+wongruxing@users.noreply.github.com>
2025-04-24 22:00:53 +0800

    Add files via upload

---
+++ line-67ef0491.js
@@ -0,0 +1,5 @@
+import{d as n,f as o,c as s,a as t,k as a,_}from"./index-0ef19f66.js";const c={class:"line"},r=n({__name:"line",props:{width:{},color:{}},setup(i){return(e,l)=>(o(),s("div",c,[t("div",{class:"inner",style:a(`width:${e.width};background: linear-gradient(
+        to right,
+        rgba(0, 0, 0, 0) 0%,
+        ${e.color} 100%
+      )`)},null,4)]))}});const p=_(r,[["__scopeId","data-v-db676efc"]]);export{p as L};
